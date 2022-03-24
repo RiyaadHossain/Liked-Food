db99bb6e80d2973f226201975d4637f2604ee72c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-24 12:28:07 +0600

    Added some hover effect

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -5,7 +5,7 @@ const Summary = ({like}) => {
     return (
         <div className='summary'>
             <div className='border'><h2>You Liked</h2></div>
-            <div className="summary-body"><p className='lead'>Like: { like.length}</p></div>
+            <div className="summary-body"><p className='lead'><strong>Like:</strong> { like.length}</p></div>
         </div>
     );
 };
