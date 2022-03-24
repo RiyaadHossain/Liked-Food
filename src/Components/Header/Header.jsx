e455f8cd62d4112b5f770015876c2f08b85f8578
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-24 10:48:20 +0600

    Ready to work

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>🥩 Vote for Your Favourite Food 🍔</h1>
+        </div>
+    );
+};
+
+export default Header;
